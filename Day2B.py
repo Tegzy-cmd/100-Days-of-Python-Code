d9d6a3cf0a97780361c6f31432f9466f78432cc1
Tegzy-cmd <77752563+Tegzy-cmd@users.noreply.github.com>
2023-03-12 17:38:02 +0100

    added Day 2 second Project: A BMI caclculator

---
+++ Day2B.py
@@ -0,0 +1,12 @@
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+newWeight = float(weight)
+newHeight = float(height)
+
+BMI = newWeight/(newHeight**2)
+
+print(int(BMI))
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 in this repo i will build 100 projects while
 learning how to code python with
 Dr Angela Yui
+ Day 1: print Hello World
+ Day 2:Built a program to sum up each digit of a two digit number, also built a BMI calculator
