8264b477fed4bd64bbddfd71c6ed757d00b23b9c
Tegzy-cmd <77752563+Tegzy-cmd@users.noreply.github.com>
2023-03-12 16:52:17 +0100

    Completed day 2 challenge to sum up each digit of a two digit number

---
+++ Day2.py
@@ -0,0 +1,12 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+firstNum = two_digit_number[0] #get first digit number
+secondNum = two_digit_number[1] #get second digit number
+
+answer = int(firstNum) + int(secondNum) # convert string values into integers
+
+print(answer) #print answer from sumation of the first and second digit
